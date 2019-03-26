42509149cb03ab065e3adc0cb75b53d00ca392a0
Reinier Isalgue <risalgue@gmail.com>
2019-03-26 09:08:14 -0400

    Initial Commit

---
+++ IHelpySwift/IHelpySwift.h
@@ -0,0 +1,19 @@
+//
+//  IHelpySwift.h
+//  IHelpySwift
+//
+//  Created by Reinier Isalgue on 3/26/19.
+//  Copyright © 2019 Reinier Isalgue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IHelpySwift.
+FOUNDATION_EXPORT double IHelpySwiftVersionNumber;
+
+//! Project version string for IHelpySwift.
+FOUNDATION_EXPORT const unsigned char IHelpySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IHelpySwift/PublicHeader.h>
+
+
